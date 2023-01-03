5597568ed91913227d79de38aada3fa2490198a0
Abiola Martins <hsemartins@gmail.com>
2023-01-03 13:52:15 +0300

    modified index.html file, style.css fie and script.js file

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const hambuger = document.querySelector(".nav-menu");
-const navMenu = document.querySelector(".toolbar_list");
+/* eslint-disable func-names */
+/* eslint-disable linebreak-style */
+const hamburger = document.querySelector('.hamburger');
+const ulCont = document.querySelector('.ul-cont');
+// const modal = document.querySelector('.popup-window');
+// const overlay = document.querySelector('.overlay');
+// const openModalBtn = document.querySelector('.card-button');
+// const closeModalBtn = document.querySelector('.closeIcon-popup');
 
-hambuger.addEventListener("click", () => {
-  hambuger.classList.toggle("active");
-  navMenu.classList.toggle("active");  
-})
+function myFunction() {
+  hamburger.classList.add('hamburger-disappear');
+  ulCont.classList.add('ul-cont-appear');
+}
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hambuger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+ulCont.addEventListener('click', ((e) => {
+  if (e.target.classList[0] === 'closeIcon') {
+    hamburger.classList.remove('hamburger-disappear');
+    ulCont.classList.remove('ul-cont-appear');
+  } else if (e.target.classList[0] === 'nav-link') {
+    hamburger.classList.remove('hamburger-disappear');
+    ulCont.classList.remove('ul-cont-appear');
+  }
+}));
+
+// const openModal = function () {
+//   overlay.classList.remove('hidden');
+//   modal.classList.remove('hidden');
+// };
+
+// openModalBtn.addEventListener('click', openModal);
+
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
+
+// closeModalBtn.addEventListener('click', closeModal);
