7a11624ffe57ee18f011b2263009ae16b50f996b
Abiola Martins <hsemartins@gmail.com>
2023-01-04 15:42:41 +0300

    Remove all old pieces of codes from inline comments as per reviewer's comment.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const ulCont = document.querySelector('.ul-cont');
-// const modal = document.querySelector('.popup-window');
-// const overlay = document.querySelector('.overlay');
-// const openModalBtn = document.querySelector('.card-button');
-// const closeModalBtn = document.querySelector('.closeIcon-popup');
 
-// function myFunction() {
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.add('hamburger-disappear');
   ulCont.classList.add('ul-cont-appear');
@@ -21,18 +17,3 @@ ulCont.addEventListener('click', ((e) => {
   }
 }));
 
-// myFunction();
-
-// const openModal = function () {
-//   overlay.classList.remove('hidden');
-//   modal.classList.remove('hidden');
-// };
-
-// openModalBtn.addEventListener('click', openModal);
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// closeModalBtn.addEventListener('click', closeModal);
